164999ad8b5b24fe7127d38b96a02cff666887cc
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-05-28 23:49:36 +0200

    Scaffolding

---
+++ cards-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cards-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'cards';
+}
